c468947b666b99943e9a3dcb91a012283530008a
Divya0822 <b24429@students.iitmandi.ac.in>
2025-11-13 20:49:49 -0500

    modified test_calculator p1

--- test_calculator.py
+++ test_calculator.py
@@ -10,13 +10,14 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
-
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_multiply(self):  
+        self.assertEqual(mul(2, 3), 6)
+        self.assertEqual(mul(-4, 5), -20)
+        self.assertEqual(mul(0, 10), 0)
+    def test_divide(self):  
+        self.assertEqual(div(10, 2), 5)
+        self.assertAlmostEqual(div(7, 2), 3.5)
+        self.assertEqual(div(-9, 3), -3)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -33,24 +34,23 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
     
-    ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_log_invalid_argument(self): 
+        with self.assertRaises(ValueError):
+            log(0, 10)
+    def test_hypotenuse(self): 
+        self.assertAlmostEqual(hypotenuse(3, 4), 5.0)
+        self.assertAlmostEqual(hypotenuse(5, 12), 13.0)
+        self.assertAlmostEqual(hypotenuse(8, 15), 17.0)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self): 
+        
+        with self.assertRaises(ValueError):
+            square_root(-4)
 
-# Do not touch this
+        self.assertAlmostEqual(square_root(9), 3.0)
+        self.assertAlmostEqual(square_root(2.25), 1.5)  
+
+
 if __name__ == "__main__":
     unittest.main()
