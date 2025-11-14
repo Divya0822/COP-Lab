0f9cb0ca20f95ce6f52caaa2000fd0762af6d5e7
anabch20 <anabelcchavez10@gmail.com>
2025-11-13 22:00:13 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -6,18 +6,20 @@ def square_root(a):
     if a<0:
         raise ValueError("")
     return math.sqrt(a)
+
 def hypotenuse(a, b):
     return math.hypot(a, b)
+
 def add(a, b):
     return a+b
 
-def sub(a,b):
+def subtract(a,b):
     return a-b
 
 def mul(a, b):
     return a*b
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("")
     return math.log(b,a)
@@ -28,17 +30,6 @@ def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Division Error")
     return b/a
-
-def subtract(a,b):
-    return a-b
-
-def logarithm(a,b):
-    if a <= 0 or a == 1 or b <= 0:
-        raise ValueError("")
-    return math.log(b,a)
-
-
-
 
 
 
